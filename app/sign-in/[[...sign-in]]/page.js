43ae912f929c906858693bffd91357609876fc5a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-30 15:42:14 +0430

    sign in

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,11 @@
+import { SignIn } from "@clerk/nextjs"
+
+const SignInPage = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage
