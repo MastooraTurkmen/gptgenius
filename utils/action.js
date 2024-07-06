8e6e38199f564034e7775f45bdd1d412db7ed39e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-06 09:49:30 +0430

    set up

--- utils/action.js
+++ utils/action.js
@@ -24,3 +24,14 @@ export const generateChatResponse = async (chatMessage) => {
     }
 
 }
+
+export const getExistingTour = async ({ city, country }) => {
+    return null
+}
+
+export const generateTourResponse = async ({ city, country }) => {
+    return null
+}
+export const createNewTour = async (tour) => {
+    return null
+}
