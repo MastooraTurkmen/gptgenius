1fa2d48e87a604305c7e340771e50504cdcef926
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-06 09:48:53 +0430

    set up new tour

---
+++ components/NewTour.jsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { getExistingTour, createNewTour, generateTourResponse } from "@/utils/action";
+import TourInfo from "./TourInfo"
+import toast from "react-hot-toast";
+
+const NewTour = () => {
+    const {mutate, isPending, data: tour} = useMutation({
+        mutationFn: async (destination) => {
+            const newTour = await generateTourResponse(destination)
+            if (newTour) {
+                return newTour
+            }
+            toast.error('No matching city found..');
+            return null;
+        }
+    })
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const destination = Object.fromEntries(formData.entries())
+        mutate(destination)
+    }
+
+    if (isPending) {
+        return <span className="loading loading-lg"></span>
+    }
+
+    return (
+        <>
+            <form action="" onSubmit={handleSubmit} className="max-w-2xl">
+                <h2 className="mb-4">select your dream destination</h2>
+                <div className="join w-full">
+                    <input
+                        type="text"
+                        className="input input-bordered join-item w-full"
+                        placeholder="city"
+                        name="city"
+                        required
+                    />
+                    <input
+                        type="text"
+                        className="input input-bordered join-item w-full"
+                        placeholder="country"
+                        name="country"
+                        required
+                    />
+                    <button className="btn btn-primary join-item" type="submit">
+                        generate tour
+                    </button>
+                </div>
+            </form>
+            <div className="mt-16">
+                {tour ? <TourInfo tour={tour} /> : null}
+            </div>
+        </>
+  )
+}
+
+export default NewTour
