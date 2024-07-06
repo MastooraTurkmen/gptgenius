fb2b758849707b12dad279cbb7a54b28acb17ac4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-06 09:04:56 +0430

    set up chat open ai

---
+++ utils/action.js
@@ -0,0 +1,26 @@
+'use server'
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+export const generateChatResponse = async (chatMessage) => {
+    try {
+        const response = await openai.chat.completions.create({
+            messages: [
+                {
+                    role: 'system',
+                    content: 'you are a helpful assistant'
+                },
+                ...chatMessage
+            ],
+            model: 'gpt-3.5-turbo',
+            temperature: 0
+        })
+        return response.choices[0].message;
+    } catch (error) {
+        return null;
+    }
+
+}
